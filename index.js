66514d77c6ed52826773b576069a9ca8082a22b2
William Quayson <wgsquayson@gmail.com>
2021-09-15 01:18:37 -0300

    feat: querying arrays of non-scalar types with and without args

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const usuarios = [
+  {
+    id: 1,
+    nome: "Will",
+    email: "will@teste.com",
+    idade: 22,
+    perfil_id: 2,
+  },
+  {
+    id: 2,
+    nome: "Adria",
+    email: "adria@teste.com",
+    idade: 20,
+    perfil_id: 2,
+  },
+  {
+    id: 3,
+    nome: "Dio",
+    email: "dio@teste.com",
+    idade: 21,
+    perfil_id: 1,
+  },
+];
+
+const perfis = [
+  {
+    id: 1,
+    nome: "Comum",
+  },
+  {
+    id: 2,
+    nome: "Administrador",
+  },
+];
+
 const typeDefs = gql`
   scalar Date
 
@@ -10,6 +45,7 @@ const typeDefs = gql`
     idade: Int
     salario: Float
     vip: Boolean
+    perfil: Perfil!
   }
 
   type Produto {
@@ -19,6 +55,11 @@ const typeDefs = gql`
     precoComDesconto: Float
   }
 
+  type Perfil {
+    id: Int!
+    nome: String!
+  }
+
   # Entry points da api
   type Query {
     helloWorld: String!
@@ -26,6 +67,10 @@ const typeDefs = gql`
     usuarioLogado: Usuario
     produtoEmDestaque: Produto
     numerosMegaSena: [Int!]!
+    usuarios: [Usuario]!
+    usuario(id: ID): Usuario
+    perfis: [Perfil]!
+    perfil(id: Int): Perfil
   }
 `;
 
@@ -33,6 +78,9 @@ const resolvers = {
   Usuario: {
     salario(usuario) {
       return usuario.salario_real;
+    },
+    perfil(usuario) {
+      return perfis.find((perfil) => perfil.id === usuario.perfil_id);
     },
   },
   Produto: {
@@ -71,6 +119,19 @@ const resolvers = {
         .fill(0)
         .map((n) => parseInt(Math.random() * 60 + 1))
         .sort(orderByNumberAsc);
+    },
+    usuarios() {
+      return usuarios;
+    },
+    usuario(_, args) {
+      const usuario = usuarios.find((usuario) => usuario.id == args.id);
+      return usuario;
+    },
+    perfis() {
+      return perfis;
+    },
+    perfil(_, { id }) {
+      return perfis.find((perfil) => perfil.id === id);
     },
   },
 };
